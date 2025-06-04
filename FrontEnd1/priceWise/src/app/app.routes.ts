d61ba8540afdfbd2380180223a1df9235b329c17
harshitjain3121 <harshitjain3121@gmail.com>
2025-06-05 00:26:28 +0530

    Auth.

--- FrontEnd1/priceWise/src/app/app.routes.ts
+++ FrontEnd1/priceWise/src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
-    {path:'',component:HomeComponent}
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'dashboard', component: HomeComponent }
 ];
